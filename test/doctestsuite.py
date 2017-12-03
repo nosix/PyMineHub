74d7181b6a81eac7478f891e79aa86700b9b1803
nosix <i.nosix@gmail.com>
2017-12-03 18:00:59 +0900

    Add test suite for doctest

---
+++ test/doctestsuite.py
@@ -0,0 +1,11 @@
+import runpy
+
+
+if __name__ == '__main__':
+    files = [
+        'pyminehub/binutil',
+        'pyminehub/network/codec',
+        'pyminehub/network/packet'
+    ]
+    for f in files:
+        runpy.run_path('../src/{}.py'.format(f), run_name='__main__')
