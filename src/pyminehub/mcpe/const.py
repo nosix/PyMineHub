dd3ac63343360c0d5d8452c310c757f0c78f194d
nosix <i.nosix@gmail.com>
2017-12-08 18:24:00 +0900

    Clarify role of the modules

---
+++ src/pyminehub/mcpe/const.py
@@ -0,0 +1,41 @@
+from enum import Enum
+
+
+class PlayStatus(Enum):
+    LOGIN_SUCCESS = 0
+    LOGIN_FAILED_CLIENT = 1
+    LOGIN_FAILED_SERVER = 2
+    PLAYER_SPAWN = 3
+    LOGIN_FAILED_INVALID_TENANT = 4
+    LOGIN_FAILED_VANILLA_EDU = 5
+    LOGIN_FAILED_EDU_VANILLA = 6
+
+
+class ResourcePackStatus(Enum):
+    REFUSED = 1
+    SEND_PACKS = 2
+    HAVE_ALL_PACKS = 3
+    COMPLETED = 4
+
+
+class Generator(Enum):
+    OLD = 0
+    INFINITE = 1
+    FLAT = 2
+
+
+class MetaDataType(Enum):
+    BYTE = 0
+    SHORT = 1
+    INT = 2
+    FLOAT = 3
+    STRING = 4
+    SLOT = 5
+    INT_VECTOR3 = 6
+    LONG = 7
+    FLOAT_VECTOR3 = 8
+
+
+class PlayerListType(Enum):
+    ADD = 0
+    REMOVE = 1
---
+++ src/pyminehub/mcpe/value.py
@@ -0,0 +1,96 @@
+from typing import NamedTuple, Tuple, Union, Optional
+
+from pyminehub.mcpe.const import *
+from pyminehub.mcpe.geometry import Vector3
+
+ConnectionRequest = NamedTuple('ConnectionRequest', [
+    ('chain', Tuple[dict, ...]),  # NOTE: dict is mutable
+    ('client', dict)  # NOTE: dict is mutable
+])
+
+PackEntry = NamedTuple('PackEntry', [
+    ('id', str),
+    ('version', str),
+    ('size', int),
+    ('unknown1', str),
+    ('unknown2', str)
+])
+
+PackStack = NamedTuple('PackStack', [
+    ('id', str),
+    ('version', str),
+    ('unknown1', str)
+])
+
+GameRule = NamedTuple('GameRule', [
+    ('name', str),
+    ('type', int),
+    ('value', Union[bool, int, float])
+])
+
+Attribute = NamedTuple('Attribute', [
+    ('min', float),
+    ('max', float),
+    ('current', float),
+    ('default', float),
+    ('name', str)
+])
+
+CommandEnum = NamedTuple('CommandEnum', [
+    ('name', str),
+    ('index', Tuple[int, ...])
+])
+
+CommandParameter = NamedTuple('CommandParameter', [
+    ('name', str),
+    ('type', int),
+    ('is_optional', bool)
+])
+
+CommandData = NamedTuple('CommandData', [
+    ('name', str),
+    ('description', str),
+    ('flags', int),
+    ('permission', int),
+    ('aliases', int),
+    ('overloads', Tuple[Tuple[CommandParameter], ...])
+])
+
+Slot = NamedTuple('Slot', [
+    ('id', int),
+    ('aux_value', Optional[int]),
+    ('nbt', Optional[bytes]),
+    ('place_on', Optional[str]),
+    ('destroy', Optional[str])
+])
+
+MetaDataValue = Union[int, float, str, Vector3, Slot]
+
+EntityMetaData = NamedTuple('EntityMetaData', [
+    ('key', int),
+    ('type', MetaDataType),
+    ('value', MetaDataValue)
+])
+
+UUID = NamedTuple('UUID', [
+    ('part1', int),
+    ('part0', int),
+    ('part3', int),
+    ('part2', int)
+])
+
+Skin = NamedTuple('Skin', [
+    ('id', str),
+    ('data', bytes),
+    ('cape', str),
+    ('geometry_name', str),
+    ('geometry_data', str)
+])
+
+PlayerListEntry = NamedTuple('PlayerListEntry', [
+    ('uuid', UUID),
+    ('entity_unique_id', Optional[int]),
+    ('user_name', Optional[str]),
+    ('skin', Optional[Skin]),
+    ('xbox_user_id', Optional[str])
+])
