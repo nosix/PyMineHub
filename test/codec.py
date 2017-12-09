f81c8d4d82fac05640681d3fd341415c892651a5
nosix <i.nosix@gmail.com>
2017-12-10 00:52:35 +0900

    Improve the design to eliminate duplication and make the results easier to read

--- test/codec.py
+++ test/codec.py
@@ -9,10 +9,12 @@ import re
 import sys
 import traceback
 from binascii import unhexlify as unhex
+from collections import namedtuple
 from os.path import dirname
 from unittest import TestCase
 
 from pyminehub import config
+from pyminehub.mcpe.network.codec import Codec
 from pyminehub.mcpe.network.codec import game_packet_codec as mcpe_game_packet_codec
 from pyminehub.mcpe.network.codec import packet_codec as mcpe_packet_codec
 from pyminehub.mcpe.network.packet import GamePacketID as MCPEGamePacketID
@@ -27,6 +29,16 @@ _logger = logging.getLogger(__name__)
 
 
 class CodecTestCase(TestCase):
+
+    def __init__(self, method_name):
+        super().__init__(method_name)
+        self._bytes_mask_enabled = True
+
+    def enable_bytes_mask(self, value):
+        self._bytes_mask_enabled = value
+
+    def is_enabled_bytes_mask(self) -> bool:
+        return self._bytes_mask_enabled
 
     def get_file_name(self, kind: str, ext='txt'):
         module_file_name = inspect.getmodule(self).__file__
@@ -43,7 +55,7 @@ class CodecTestCase(TestCase):
         self._file_handler.close()
 
 
-def interrupt_for_pycharm(exc: Exception, called, packet_info=None):
+def interrupt_for_pycharm(exc: Exception, called, packet_info: str=None):
     if type(exc).__name__ == 'EqualsAssertionError':
         attrs = exc.__dict__
         if called is not None:
@@ -69,18 +81,67 @@ class _WrappedException(Exception):
         self.tb = tb
 
 
+_ASCII_CHARS = set(c for c in range(0x20, 0x7f))
+_ASCII_CHARS_FOR_DOUBLE_QUOTE = _ASCII_CHARS - {0x22, 0x5c, 0x5d}  # " - \
+_ASCII_CHARS_FOR_SINGLE_QUOTE = _ASCII_CHARS - {0x27, 0x5c, 0x5d}  # ' - \
+
+_BYTES_REGEXP_FOR_DOUBLE_QUOTE = re.compile(
+    r'(b"[{}\-\\\]]*?")'.format(''.join(chr(c) for c in _ASCII_CHARS_FOR_DOUBLE_QUOTE)))
+_BYTES_REGEXP_FOR_SINGLE_QUOTE = re.compile(
+    r"(b'[{}\-\\\]]*?')".format(''.join(chr(c) for c in _ASCII_CHARS_FOR_SINGLE_QUOTE)))
+
+
 class PacketAssertion:
+
+    _MAX_BYTES_LEN = 16
 
     def __init__(self, stack_depth=3):
         called_line = traceback.format_stack()[-stack_depth]
         m = re.search(r'File "(.+)/.+\.py"', called_line)
         self.called_line = called_line.replace(m[1], '.', 1)
+        self._data = None
+        self._packet = None
+        self._codec = None
 
-    def is_correct_on(self, test_case: TestCase, data: bytes):
+    def record_decoded(self, data: bytes, packet: namedtuple, codec: Codec) -> None:
+        self._data = data
+        self._packet = packet
+        self._codec = codec
+
+    def has_record(self):
+        return not (self._data is None or self._packet is None or self._codec is None)
+
+    def is_correct_on(self, test_case: CodecTestCase, data: bytes) -> int:
         raise NotImplemented
 
-    def verify_on(self, test_case: TestCase):
-        raise NotImplemented
+    def verify_on(self, test_case: CodecTestCase) -> None:
+        assert self.has_record()
+        self.verify_children(test_case)
+        data = self._codec.encode(self._packet)
+
+        def reduce_bytes(m):
+            bytes_data = eval(m[1])  # type: bytes
+            length = len(bytes_data)
+            return repr(bytes_data) if length <= self._MAX_BYTES_LEN else '[{} bytes]'.format(length)
+
+        if test_case.is_enabled_bytes_mask():
+            packet_str = str(self._packet).replace(r'\"', r'\x22').replace(r"\'", r'\x27')
+            packet_str = _BYTES_REGEXP_FOR_DOUBLE_QUOTE.sub(reduce_bytes, packet_str)
+            packet_str = _BYTES_REGEXP_FOR_SINGLE_QUOTE.sub(reduce_bytes, packet_str)
+        else:
+            packet_str = str(self._packet)
+        try:
+            test_case.assertEqual(
+                self._data.hex(), data.hex(), '\n{}\n  {}'.format(self.called_line, packet_str))
+            _logger.info('%s\n  -> %s\n%s', packet_str, self._data.hex(), self.called_line)
+        except AssertionError as e:
+            self.verify_error_hook(test_case, e, data)
+
+    def verify_children(self, test_case: CodecTestCase) -> None:
+        pass
+
+    def verify_error_hook(self, test_case: CodecTestCase, exc: AssertionError, data: bytes) -> None:
+        raise exc
 
 
 class GamePacket(PacketAssertion):
@@ -91,22 +152,14 @@ class GamePacket(PacketAssertion):
         """
         super().__init__()
         self._packet_id = packet_id
-        self._data = None
-        self._mcpe_game_packet = None
 
-    def is_correct_on(self, test_case: TestCase, data: bytes):
+    def is_correct_on(self, test_case: CodecTestCase, data: bytes):
         context = PacketCodecContext()
-        self._mcpe_game_packet = mcpe_game_packet_codec.decode(data, context)
-        test_case.assertEqual(self._packet_id, MCPEGamePacketID(self._mcpe_game_packet.id))
+        packet = mcpe_game_packet_codec.decode(data, context)
+        test_case.assertEqual(self._packet_id, MCPEGamePacketID(packet.id))
         test_case.assertEqual(len(data), context.length)
-        self._data = data[:context.length]
-
-    def verify_on(self, test_case: TestCase):
-        assert self._data is not None and self._mcpe_game_packet is not None
-        data = mcpe_game_packet_codec.encode(self._mcpe_game_packet)
-        test_case.assertEqual(
-            self._data.hex(), data.hex(), '\n{}\n  {}'.format(self.called_line, self._mcpe_game_packet))
-        _logger.info('%s\n  -> %s\n%s', self._mcpe_game_packet, self._data.hex(), self.called_line)
+        self.record_decoded(data[:context.length], packet, mcpe_game_packet_codec)
+        return context.length
 
 
 class Batch(PacketAssertion):
@@ -115,47 +168,36 @@ class Batch(PacketAssertion):
         """ Batch packet validator."""
         super().__init__()
         self._assertions = []
-        self._data = None
-        self._mcpe_packet = None
 
     def that_has(self, *game_packet: GamePacket):
         self._assertions.extend(game_packet)
         return self
 
-    def is_correct_on(self, test_case: TestCase, data: bytes):
+    def is_correct_on(self, test_case: CodecTestCase, data: bytes):
         context = PacketCodecContext()
-        self._mcpe_packet = mcpe_packet_codec.decode(data, context)
-        test_case.assertEqual(MCPEPacketID.BATCH, MCPEPacketID(self._mcpe_packet.id))
+        packet = mcpe_packet_codec.decode(data, context)
+        test_case.assertEqual(MCPEPacketID.BATCH, MCPEPacketID(packet.id))
         test_case.assertEqual(len(data), context.length)
-        test_case.assertEqual(len(self._assertions), len(self._mcpe_packet.payloads))
-        for payload, assertion in zip(self._mcpe_packet.payloads, self._assertions):
+        test_case.assertEqual(len(self._assertions), len(packet.payloads))
+        packet_info = '  MCPE packet {}'.format(packet)
+        for payload, assertion in zip(packet.payloads, self._assertions):
             try:
                 assertion.is_correct_on(test_case, payload)
             except Exception as e:
-                packet_info = '  MCPE packet {}'.format(self._mcpe_packet)
                 interrupt_for_pycharm(e, assertion.called_line, packet_info)
                 message = '{} occurred while testing\n{}{}'.format(repr(e), assertion.called_line, packet_info)
                 raise _WrappedException(e, sys.exc_info()[2], message) from None
-        self._data = data[:context.length]
+        self.record_decoded(data[:context.length], packet, mcpe_packet_codec)
+        return context.length
 
-    def verify_on(self, test_case: TestCase):
-        assert self._data is not None and self._mcpe_packet is not None
+    def verify_children(self, test_case: CodecTestCase):
         for assertion in self._assertions:
             assertion.verify_on(test_case)
-        data = mcpe_packet_codec.encode(self._mcpe_packet)
-        try:
-            test_case.assertEqual(
-                self._data.hex(), data.hex(), '\n{}\n  {}'.format(self.called_line, self._mcpe_packet))
-            import re
-            reduced = re.sub(
-                r"bytearray\((b'.*')\)",
-                lambda m: '[{} bytes]'.format(len(repr(m[1]))),
-                str(self._mcpe_packet))
-            _logger.info('%s\n  -> %s\n%s', reduced, self._data.hex(), self.called_line)
-        except AssertionError as e:
-            print('There may be differences in compression results:', e, file=sys.stderr)
-            self.is_correct_on(test_case, data)
-            self.verify_on(test_case)
+
+    def verify_error_hook(self, test_case: CodecTestCase, exc: AssertionError, data: bytes):
+        print('There may be differences in compression results:', exc, file=sys.stderr)
+        self.is_correct_on(test_case, data)
+        self.verify_on(test_case)
 
 
 class Capsule(PacketAssertion):
@@ -167,38 +209,32 @@ class Capsule(PacketAssertion):
         super().__init__()
         self._capsule_id = capsule_id
         self._assertion = None
-        self._data = None
-        self._capsule = None
 
     def that_has(self, batch: Batch):
         self._assertion = batch
         return self
 
-    def is_correct_on(self, test_case: TestCase, data: bytes):
+    def is_correct_on(self, test_case: CodecTestCase, data: bytes):
         context = PacketCodecContext()
-        self._capsule = raknet_capsule_codec.decode(data, context)
-        test_case.assertEqual(self._capsule_id, RakNetCapsuleID(self._capsule.id))
+        capsule = raknet_capsule_codec.decode(data, context)
+        test_case.assertEqual(self._capsule_id, RakNetCapsuleID(capsule.id))
+        capsule_info = '  RakNet encapsulation {}'.format(capsule)
         if self._assertion is not None:
             try:
-                self._assertion.is_correct_on(test_case, self._capsule.payload)
+                self._assertion.is_correct_on(test_case, capsule.payload)
             except _WrappedException as e:
-                message = '{}\n  RakNet encapsulation {}'.format(e.args[0], self._capsule)
+                message = '{}\n'.format(e.args[0], capsule_info)
                 raise _WrappedException(e.exc, e.tb, message) from None
             except Exception as e:
-                packet_info = '  RakNet encapsulation {}'.format(self._capsule)
-                interrupt_for_pycharm(e, self._assertion.called, packet_info)
-                message = '{} occurred while testing\n{}'.format(repr(e), self._assertion.called, packet_info)
+                interrupt_for_pycharm(e, self._assertion.called, capsule_info)
+                message = '{} occurred while testing\n{}'.format(repr(e), self._assertion.called, capsule_info)
                 raise _WrappedException(e, sys.exc_info()[2], message) from None
-        self._data = data[:context.length]
+        self.record_decoded(data[:context.length], capsule, raknet_capsule_codec)
         return context.length
 
-    def verify_on(self, test_case: TestCase):
-        assert self._data is not None and self._capsule is not None
+    def verify_children(self, test_case: CodecTestCase):
         if self._assertion is not None:
             self._assertion.verify_on(test_case)
-        data = raknet_capsule_codec.encode(self._capsule)
-        test_case.assertEqual(self._data.hex(), data.hex(), '\n{}\n  {}'.format(self.called_line, self._capsule))
-        _logger.info('%s\n  -> %s\n%s', self._capsule, self._data.hex(), self.called_line)
 
 
 class RakNetPacket(PacketAssertion):
@@ -210,42 +246,43 @@ class RakNetPacket(PacketAssertion):
         super().__init__()
         self._packet_id = packet_id
         self._assertions = []
-        self._data = None
-        self._raknet_packet = None
 
     def that_has(self, *capsule: Capsule):
         self._assertions.extend(capsule)
         return self
 
-    def _test_raknet_packet(self, test_case: TestCase, data: bytes):
+    def _test_raknet_packet(self, test_case: CodecTestCase, data: bytes):
         assert self._packet_id is not None
         context = PacketCodecContext()
-        self._raknet_packet = raknet_packet_codec.decode(data, context)
-        test_case.assertEqual(self._packet_id, RakNetPacketID(self._raknet_packet.id), self._raknet_packet)
+        packet = raknet_packet_codec.decode(data, context)
+        test_case.assertEqual(self._packet_id, RakNetPacketID(packet.id), packet)
         test_case.assertEqual(len(data), context.length)
+        self.record_decoded(data, packet, raknet_packet_codec)
+        return context.length, packet
 
-    def _test_raknet_encapsulation(self, test_case: TestCase):
-        assert self._raknet_packet is not None
-        data = self._raknet_packet.payload
+    def _test_raknet_encapsulation(self, test_case: CodecTestCase, packet: namedtuple):
+        assert self.has_record()
+        data = packet.payload
+        packet_info = '  RakNet packet {}'.format(packet)
         payload_length = 0
         for assertion in self._assertions:
             try:
                 payload_length += assertion.is_correct_on(test_case, data[payload_length:])
             except _WrappedException as e:
-                message = '{}\n  RakNet packet {}'.format(e.args[0], self._raknet_packet)
+                message = '{}\n{}'.format(e.args[0], packet_info)
                 raise _WrappedException(e.exc, e.tb, message) from None
             except Exception as e:
-                packet_info = '  RakNet packet {}'.format(self._raknet_packet)
                 interrupt_for_pycharm(e, assertion.called_line, packet_info)
                 message = '{} occurred while testing\n{}{}'.format(repr(e), assertion.called_line, packet_info)
                 raise _WrappedException(e, sys.exc_info()[2], message) from None
         test_case.assertEqual(len(data), payload_length, 'Capsule may be missing with "{}"'.format(data.hex()))
 
-    def is_correct_on(self, test_case: TestCase, data: bytes):
+    def is_correct_on(self, test_case: CodecTestCase, data: bytes):
         self._data = data
         try:
-            self._test_raknet_packet(test_case, data)
-            self._test_raknet_encapsulation(test_case)
+            length, packet = self._test_raknet_packet(test_case, data)
+            self._test_raknet_encapsulation(test_case, packet)
+            return length
         except _WrappedException as e:
             message = '{}'.format(e.args[0])
             raise _WrappedException(e.exc, e.tb, message) from None
@@ -254,13 +291,9 @@ class RakNetPacket(PacketAssertion):
             message = '{} occurred while testing\n{}'.format(repr(e), self.called_line)
             raise _WrappedException(e, sys.exc_info()[2], message) from None
 
-    def verify_on(self, test_case: TestCase):
-        assert self._raknet_packet is not None
+    def verify_children(self, test_case: CodecTestCase):
         for assertion in self._assertions:
             assertion.verify_on(test_case)
-        data = raknet_packet_codec.encode(self._raknet_packet)
-        test_case.assertEqual(self._data.hex(), data.hex(), '\n{}\n  {}'.format(self.called_line, self._raknet_packet))
-        _logger.info('%s\n  -> %s\n%s', self._raknet_packet, self._data.hex(), self.called_line)
 
 
 class EncodedData:
@@ -277,7 +310,7 @@ class EncodedData:
         self._assertion = assertion
         return self
 
-    def is_correct_on(self, test_case: TestCase, and_verified_with_encoded_data=False):
+    def is_correct_on(self, test_case: CodecTestCase, and_verified_with_encoded_data=False):
         try:
             self._assertion.is_correct_on(test_case, self._data)
         except _WrappedException as e:
