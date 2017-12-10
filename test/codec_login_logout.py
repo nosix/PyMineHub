eac5da90e7de7ffad2f4e1a4c4c82c7e13dbe9a0
nosix <i.nosix@gmail.com>
2017-12-10 17:38:30 +0900

    Use client or server letter and sequence number as test name

--- test/codec_login_logout.py
+++ test/codec_login_logout.py
@@ -3,7 +3,45 @@ from codec import *
 
 class LoginLogoutTestCase(CodecTestCase):
 
-    def test_login_logout_00(self):
+    def test_login_logout_c00(self):
+        assertion = EncodedData(
+            '8400000040009000000009869e0aed5f1a87ae00000000003d380300'
+        ).is_(
+            RakNetPacket(RakNetPacketID.CUSTOM_PACKET_4).that_has(
+                Capsule(RakNetCapsuleID.RELIABLE).that_has(
+                    ConnectionPacket(MCPEPacketID.CONNECTION_REQUEST)
+                )
+            )
+        )
+        assertion.is_correct_on(self, and_verified_with_encoded_data=False)
+
+    def test_login_logout_s01(self):
+        assertion = EncodedDataInFile(self).is_(
+            RakNetPacket(RakNetPacketID.CUSTOM_PACKET_4).that_has(
+                Capsule(RakNetCapsuleID.UNRELIABLE).that_has(
+                    ConnectionPacket(MCPEPacketID.CONNECTION_REQUEST_ACCEPTED)
+                )
+            )
+        )
+        assertion.is_correct_on(self, and_verified_with_encoded_data=False)
+
+    def test_login_logout_c01(self):
+        assertion = EncodedDataInFile(self).is_(
+            RakNetPacket(RakNetPacketID.CUSTOM_PACKET_4).that_has(
+                Capsule(RakNetCapsuleID.UNRELIABLE).that_has(
+                    ConnectionPacket(MCPEPacketID.CONNECTED_PONG)
+                ),
+                Capsule(RakNetCapsuleID.RELIABLE_ORDERED).that_has(
+                    ConnectionPacket(MCPEPacketID.NEW_INCOMING_CONNECTION)
+                ),
+                Capsule(RakNetCapsuleID.UNRELIABLE).that_has(
+                    ConnectionPacket(MCPEPacketID.CONNECTED_PING)
+                ),
+            )
+        )
+        assertion.is_correct_on(self, and_verified_with_encoded_data=False)
+
+    def test_login_logout_c0c(self):
         assertion = EncodedDataInFile(self).is_(
             Batch().that_has(
                 GamePacket(MCPEGamePacketID.LOGIN)
@@ -11,7 +49,7 @@ class LoginLogoutTestCase(CodecTestCase):
         )
         assertion.is_correct_on(self, and_verified_with_encoded_data=False)
 
-    def test_login_logout_01(self):
+    def test_login_logout_s04(self):
         assertion = EncodedData(
             '840400006000a000000000000000fe7801010800f7ff0702000000000000004e'
             '000a6000a801000001000000fe7801010900f6ff080600000000000000008100'
@@ -32,7 +70,7 @@ class LoginLogoutTestCase(CodecTestCase):
         )
         assertion.is_correct_on(self, and_verified_with_encoded_data=True)
 
-    def test_login_logout_02(self):
+    def test_login_logout_c0d(self):
         config.set_config(batch_compress_threshold=0)  # TODO
         assertion = EncodedData(
             '840d00006000800b000002000000fe78da63e360606066600000006a0012'
@@ -47,7 +85,7 @@ class LoginLogoutTestCase(CodecTestCase):
         )
         assertion.is_correct_on(self, and_verified_with_encoded_data=True)
 
-    def test_login_logout_03(self):
+    def test_login_logout_s05(self):
         assertion = EncodedData(
             '8405000060009802000002000000fe7801010700f8ff06070000000000005b00'
             '0e'
@@ -62,7 +100,7 @@ class LoginLogoutTestCase(CodecTestCase):
         )
         assertion.is_correct_on(self, and_verified_with_encoded_data=True)
 
-    def test_login_logout_04(self):
+    def test_login_logout_s09(self):
         assertion = EncodedDataInFile(self).is_(
             RakNetPacket(RakNetPacketID.CUSTOM_PACKET_4).that_has(
                 Capsule(RakNetCapsuleID.RELIABLE_ORDERED).that_has(
@@ -94,7 +132,7 @@ class LoginLogoutTestCase(CodecTestCase):
         )
         assertion.is_correct_on(self, and_verified_with_encoded_data=True)
 
-    def test_login_logout_05(self):
+    def test_login_logout_s0a(self):
         assertion = EncodedData(
             '840a000060021808000008000000fe7801013700c8ff36270000010700078080'
             'c680808030070100002b01900104040f4d617474654d757373656c3336323026'
@@ -123,7 +161,7 @@ class LoginLogoutTestCase(CodecTestCase):
         )
         assertion.is_correct_on(self, and_verified_with_encoded_data=True)
 
-    def test_login_logout_06(self):
+    def test_login_logout_s0c(self):
         assertion = EncodedDataInFile(self).is_(
             Batch().that_has(
                 GamePacket(MCPEGamePacketID.INVENTORY_CONTENT)
@@ -131,7 +169,7 @@ class LoginLogoutTestCase(CodecTestCase):
         )
         assertion.is_correct_on(self, and_verified_with_encoded_data=True)
 
-    def test_login_logout_07(self):
+    def test_login_logout_s0d(self):
         assertion = EncodedData(
             '840d00006000a80d00000c000000fe7801010900f6ff081f0000010000000001'
             '4e00296000980e00000d000000fe7801010700f8ff06320000000000015d0039'
@@ -151,7 +189,7 @@ class LoginLogoutTestCase(CodecTestCase):
         )
         assertion.is_correct_on(self, and_verified_with_encoded_data=True)
 
-    def test_login_logout_08(self):
+    def test_login_logout_s0f(self):
         assertion = EncodedDataInFile(self).is_(
             Batch().that_has(
                 GamePacket(MCPEGamePacketID.PLAYER_LIST)
@@ -159,7 +197,7 @@ class LoginLogoutTestCase(CodecTestCase):
         )
         assertion.is_correct_on(self, and_verified_with_encoded_data=True)
 
-    def test_login_logout_09(self):
+    def test_login_logout_s20(self):
         assertion = EncodedDataInFile(self).is_(
             Batch().that_has(
                 GamePacket(MCPEGamePacketID.CRAFTING_DATA)
@@ -167,7 +205,7 @@ class LoginLogoutTestCase(CodecTestCase):
         )
         assertion.is_correct_on(self, and_verified_with_encoded_data=True)
 
-    def test_login_logout_10(self):
+    def test_login_logout_c12(self):
         assertion = EncodedData(
             '841200006000700e000004000000fe78da63716560100000013d005a'
         ).is_(
@@ -181,7 +219,7 @@ class LoginLogoutTestCase(CodecTestCase):
         )
         assertion.is_correct_on(self, and_verified_with_encoded_data=True)
 
-    def test_login_logout_11(self):
+    def test_login_logout_s21(self):
         assertion = EncodedData(
             '8421000060008822000010000000fe7801010500faff04460000100141005b'
         ).is_(
@@ -195,7 +233,7 @@ class LoginLogoutTestCase(CodecTestCase):
         )
         assertion.is_correct_on(self, and_verified_with_encoded_data=True)
 
-    def test_login_logout_12(self):
+    def test_login_logout_s22(self):
         assertion = EncodedData(
             '842200006000b823000011000000fe7801010b00f4ff0a150000f60337e20303'
             '300cf10268'
@@ -210,7 +248,7 @@ class LoginLogoutTestCase(CodecTestCase):
         )
         assertion.is_correct_on(self, and_verified_with_encoded_data=True)
 
-    def test_login_logout_13(self):
+    def test_login_logout_s23(self):
         assertion = EncodedDataInFile(self).is_(
             RakNetPacket(RakNetPacketID.CUSTOM_PACKET_4).that_has(
                 Capsule(RakNetCapsuleID.RELIABLE_ORDERED).that_has(
@@ -222,7 +260,7 @@ class LoginLogoutTestCase(CodecTestCase):
         )
         assertion.is_correct_on(self, and_verified_with_encoded_data=True)
 
-    def test_login_logout_14(self):
+    def test_login_logout_s85(self):
         assertion = EncodedData(
             '84850100600008a002008402000015'
         ).is_(
