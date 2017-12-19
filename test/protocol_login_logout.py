6b8b14fa6bcebcb8a4b00ae106f378843ce3ba45
nosix <i.nosix@gmail.com>
2017-12-19 17:26:24 +0900

    Add test case for SetEntityData, InventoryContent, MobEquipment, PlayerList and CraftingData packet

--- test/protocol_login_logout.py
+++ test/protocol_login_logout.py
@@ -173,6 +173,97 @@ class LoginLogoutTestCase(UnconnectedTestCase):
                 )
             ]
         })
+        received_data = self.proxy.next_moment()
+        self.assert_that(received_data, {
+            self._CLIENT_ADDRESS[0]: [
+                # 06
+                EncodedData(self.data.that_is_response_of('some_data')).is_(
+                    RakNetPacket(RakNetPacketType.CUSTOM_PACKET_4).that_has(
+                        RakNetFrame(RakNetFrameType.RELIABLE_ORDERED).that_has(
+                            Batch().that_has(
+                                GamePacket(GamePacketType.SET_ENTITY_DATA)
+                            )
+                        ),
+                        RakNetFrame(RakNetFrameType.RELIABLE_ORDERED).that_has(
+                            Batch().that_has(
+                                GamePacket(GamePacketType.INVENTORY_CONTENT)
+                            )
+                        ),
+                        RakNetFrame(RakNetFrameType.RELIABLE_ORDERED).that_has(
+                            Batch().that_has(
+                                GamePacket(GamePacketType.INVENTORY_CONTENT)
+                            )
+                        )
+                    )
+                ),
+                # 07
+                EncodedData(self.data.that_is_response_of('some_data')).is_(
+                    RakNetPacket(RakNetPacketType.CUSTOM_PACKET_4).that_has(
+                        RakNetFrame(RakNetFrameType.RELIABLE_ORDERED_HAS_SPLIT)
+                    )
+                ),
+                # 08
+                EncodedData(self.data.that_is_response_of('some_data')).is_(
+                    RakNetPacket(RakNetPacketType.CUSTOM_PACKET_4).that_has(
+                        RakNetFrame(RakNetFrameType.RELIABLE_ORDERED_HAS_SPLIT).that_has(
+                            Batch().that_has(
+                                GamePacket(GamePacketType.INVENTORY_CONTENT)
+                            )
+                        )
+                    )
+                ),
+                # 09
+                EncodedData(self.data.that_is_response_of('some_data')).is_(
+                    RakNetPacket(RakNetPacketType.CUSTOM_PACKET_4).that_has(
+                        RakNetFrame(RakNetFrameType.RELIABLE_ORDERED).that_has(
+                            Batch().that_has(
+                                GamePacket(GamePacketType.MOB_EQUIPMENT)
+                            )
+                        ),
+                        RakNetFrame(RakNetFrameType.RELIABLE_ORDERED_HAS_SPLIT).that_has(
+                            Batch().that_has(
+                                GamePacket(GamePacketType.INVENTORY_SLOT)
+                            )
+                        )
+                    )
+                ),
+                # 0a
+                EncodedData(self.data.that_is_response_of('some_data')).is_(
+                    RakNetPacket(RakNetPacketType.CUSTOM_PACKET_4).that_has(
+                        RakNetFrame(RakNetFrameType.RELIABLE_ORDERED_HAS_SPLIT)
+                    )
+                ),
+                # 0b
+                EncodedData(self.data.that_is_response_of('some_data')).is_(
+                    RakNetPacket(RakNetPacketType.CUSTOM_PACKET_4).that_has(
+                        RakNetFrame(RakNetFrameType.RELIABLE_ORDERED_HAS_SPLIT).that_has(
+                            Batch().that_has(
+                                GamePacket(GamePacketType.PLAYER_LIST)
+                            )
+                        )
+                    )
+                ),
+                # 0c-1b
+                *[
+                    EncodedData(self.data.that_is_response_of('some_data')).is_(
+                        RakNetPacket(RakNetPacketType.CUSTOM_PACKET_4).that_has(
+                            RakNetFrame(RakNetFrameType.RELIABLE_ORDERED_HAS_SPLIT)
+                        )
+                    )
+                    for _ in range(16)
+                ],
+                # 1c
+                EncodedData(self.data.that_is_response_of('some_data')).is_(
+                    RakNetPacket(RakNetPacketType.CUSTOM_PACKET_4).that_has(
+                        RakNetFrame(RakNetFrameType.RELIABLE_ORDERED_HAS_SPLIT).that_has(
+                            Batch().that_has(
+                                GamePacket(GamePacketType.CRAFTING_DATA)
+                            )
+                        ),
+                    )
+                )
+            ]
+        })
 
 
 if __name__ == '__main__':
