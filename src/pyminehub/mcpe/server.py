6ffab654ff180fb496fdcb1f86c42a7a70935a5b
nosix <i.nosix@gmail.com>
2017-12-21 11:06:31 +0900

    Fix bug

--- src/pyminehub/mcpe/server.py
+++ src/pyminehub/mcpe/server.py
@@ -1,9 +1,11 @@
 from pyminehub.mcpe.network import MCPEHandler
+from pyminehub.mcpe.world import run as run_world
 from pyminehub.raknet import run as run_raknet
 
 
 def run() -> None:
-    run_raknet(MCPEHandler())
+    proxy = run_world()
+    run_raknet(MCPEHandler(proxy))
 
 
 if __name__ == '__main__':
