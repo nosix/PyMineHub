eae24ec804fc8c1f3658a41563bd27de65c5b4f1
nosix <i.nosix@gmail.com>
2017-12-23 20:11:40 +0900

    Fix an error when executed atr terminal

--- test/doctestsuite.py
+++ test/doctestsuite.py
@@ -1,10 +1,14 @@
+import inspect
 import runpy
 import unittest
+from os.path import dirname
 
 
 class DocTestCase(unittest.TestCase):
 
     def test_doctest(self):
+        module_file_name = inspect.getmodule(self).__file__
+        test_dir = dirname(module_file_name)
         module_path = [
             'pyminehub/config',
             'pyminehub/binutil/converter',
@@ -16,7 +20,8 @@ class DocTestCase(unittest.TestCase):
         ]
         for path in module_path:
             with self.subTest(module=path):
-                result = runpy.run_path('../src/{}.py'.format(path), run_name='__main__')['doctest_result']
+                result = runpy.run_path(
+                    '{}/../src/{}.py'.format(test_dir, path), run_name='__main__')['doctest_result']
                 self.assertEqual(0, result.failed, result)
 
 
