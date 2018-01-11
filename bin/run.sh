e0bf967770c5aab3931c558c81954f8c06c54626
nosix <i.nosix@gmail.com>
2018-01-11 14:12:37 +0900

    Add profiling option

--- bin/run.sh
+++ bin/run.sh
@@ -1,11 +1,30 @@
 #!/usr/bin/env bash
 
+usage ()
+{
+  CMD_NAME=`basename $0`
+  echo "Usage: $CMD_NAME [-p]"
+  exit 1
+}
+
+PROFILER=
+
+while getopts ph OPT
+do
+    case ${OPT} in
+        p)  PROFILER="-m cProfile -o run.pstat"
+            ;;
+        h)  usage
+            ;;
+    esac
+done
+
 BASEDIR=$(dirname "$0")/..
 
 ${BASEDIR}/bin/checkver.sh || exit 1
 
 export PYTHONPATH=${PYTHONPATH}:${BASEDIR}/src
 
-python3 ${BASEDIR}/src/pyminehub/mcpe/main/server.py
+python3 ${PROFILER} ${BASEDIR}/src/pyminehub/mcpe/main/server.py
 
 exit 0
--- bin/test.sh
+++ bin/test.sh
@@ -1,11 +1,30 @@
 #!/usr/bin/env bash
 
+usage ()
+{
+  CMD_NAME=`basename $0`
+  echo "Usage: $CMD_NAME [-p]"
+  exit 1
+}
+
+PROFILER=
+
+while getopts ph OPT
+do
+    case ${OPT} in
+        p)  PROFILER="-m cProfile -o run.pstat"
+            ;;
+        h)  usage
+            ;;
+    esac
+done
+
 BASEDIR=$(dirname "$0")/..
 
 ${BASEDIR}/bin/checkver.sh || exit 1
 
 export PYTHONPATH=${PYTHONPATH}:${BASEDIR}/src:${BASEDIR}/tool:${BASEDIR}/test
 
-python3 ${BASEDIR}/test/all.py
+python3 ${PROFILER} ${BASEDIR}/test/all.py
 
 exit 0
