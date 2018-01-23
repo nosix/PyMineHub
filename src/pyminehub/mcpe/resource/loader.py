7b7527c22e0ded998bc0f35b0d6555e3cecb2369
nosix <i.nosix@gmail.com>
2018-01-23 14:10:04 +0900

    Add __all__ in resource module of mcpe

--- src/pyminehub/mcpe/resource/loader.py
+++ src/pyminehub/mcpe/resource/loader.py
@@ -1,6 +1,12 @@
 from pkgutil import get_data
 
 
+__all__ = [
+    'CRAFTING_DATA_RECIPE',
+    'INVENTORY_CONTENT_ITEMS121'
+]
+
+
 CRAFTING_DATA_RECIPE = get_data(__package__, 'crafting-data-recipe.dat')
 
 INVENTORY_CONTENT_ITEMS121 = get_data(__package__, 'inventory-content-items121.dat')
